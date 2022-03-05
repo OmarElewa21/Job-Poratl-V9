460452521c41878456d9dab056e7551759732587
Hashtechs <hashtechs.stack@gmail.com>
2022-03-05 15:46:10 +0200

    add link to dashboard

--- resources/views/emails/quizzes/new_quiz_notify.blade.php
+++ resources/views/emails/quizzes/new_quiz_notify.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Hello,
 
-### You have a new pending quiz, waiting on your dashboard. Please login to take the quiz.
+### You have a new pending quiz, waiting on your [dashboard]({{env('APP_URL') . '/users/candidate-login'}}). Please login to take the quiz.
 
 Regards,<br>
 {{ config('app.name') }}
